953410c6d2dbde5a9c440d340265970d0d54a3d4
Elias Hernandis <eliashernandis@gmail.com>
2016-08-20 23:02:09 +0200

    Add paperclip styles

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,7 +1,8 @@
 class Poster < ApplicationRecord
   belongs_to :carousel
 
-  has_attached_file :background
+  has_attached_file :background,
+    styles: { preview: '600x420', carousel: '1528x1080' }
 
   validates :title, presence: true
   validates_attachment :background,
