a29a90d6d828c3799ae18a4e5298039c2e5738f8
Elias Hernandis <eliashernandis@gmail.com>
2016-08-25 14:55:50 +0200

    reorder seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Settings.delete_all
-User.delete_all
-
 Poster.delete_all
 Carousel.delete_all
+
+Settings.delete_all
+User.delete_all
 
 u = User.create(name: 'Elias Hernandis', email: 'test@me.com', password: 'helloworld', password_confirmation: 'helloworld')
 u.confirm
