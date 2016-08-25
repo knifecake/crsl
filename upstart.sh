f1fbda153c2e0a272dea30a6e79d0e6bb5950f85
Elias Hernandis <eliashernandis@gmail.com>
2016-08-25 14:30:50 +0200

    Upstart script

--- upstart.sh
+++ upstart.sh
@@ -3,6 +3,8 @@ echo "Precompiling assets and migrating database..."
 rails assets:precompile
 rails db:migrate
 
+mkdir -p tmp/sockets tmp/pids
+
 echo "Starting puma..."
 puma --daemon --config config/puma.rb
 
