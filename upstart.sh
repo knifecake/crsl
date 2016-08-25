29235394f901597665907d24b45933d53581c207
Elias Hernandis <eliashernandis@gmail.com>
2016-08-25 14:46:12 +0200

    Sendgrid

--- upstart.sh
+++ upstart.sh
@@ -5,6 +5,10 @@ rails db:migrate
 
 mkdir -p tmp/sockets tmp/pids
 
+if [ -f ".rbenv-vars" ]; then
+  echo "Rails need a secret key base in .rbenv-vars."; exit 1
+fi
+
 echo "Starting puma..."
 puma --daemon --config config/puma.rb
 
